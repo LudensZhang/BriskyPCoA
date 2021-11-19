2ba2d71d38ede9d5a48c3c73e06df9debe34273e
HaohongZhang <iceqhdrop@outlook.com>
2021-11-19 01:41:47 +0000

    update BriskyPCoA.py

--- BriskyPCoA.py
+++ BriskyPCoA.py
@@ -8,4 +8,8 @@ from scipy.spatial.distance import pdist, squareform
 
 if __name__ == '__main__':
     parser = argparse.ArguementParser()
-    parser.add_argument('-i', '--abundance', type='str', defaut='./data/abundance.csv', )
+    parser.add_argument('-i', '--abundance', type='str', defaut='./data/abundance.csv', help='Abundance data, entries represent taxas and columns represent samples')
+    parser.add_argument('-l', '--lables', type='str', defaut='./data/metadata.csv', help='Labels of samples, first column represent SampleID and second column represent labels')
+    parser.add_argument('-d', '--distance', type='str', defaut='JSD', help='Optional distance, support Braycurties and JSD')
+    parser.add_argument('-o', '--output', type='str', defaut='./', help='Output path of visulizing result'
+                        )
