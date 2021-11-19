f0b8935a10a346b955e614cc5dff09ecf393d517
HaohongZhang <iceqhdrop@outlook.com>
2021-11-19 01:29:45 +0000

    update BriskyPCoA.py

--- BriskyPCoA.py
+++ BriskyPCoA.py
@@ -2,3 +2,10 @@ import pandas as pd
 import numpy as np
 from skbio.diversity import beta_diversity
 from skbio.stats.ordination import pcoa
+import argparse
+from scipy.spatial.distance import pdist, squareform
+
+
+if __name__ == '__main__':
+    parser = argparse.ArguementParser()
+    parser.add_argument('-i', '--abundance', type='str', defaut='./data/abundance.csv', )
